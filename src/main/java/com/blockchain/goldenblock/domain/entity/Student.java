935086db86cc85c1015caa20a3de01b8279ac492
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-08-05 14:30:34 +0900

    add jwt

--- src/main/java/com/blockchain/goldenblock/domain/entity/Student.java
+++ src/main/java/com/blockchain/goldenblock/domain/entity/Student.java
@@ -1,16 +1,8 @@
 package com.blockchain.goldenblock.domain.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "number")
