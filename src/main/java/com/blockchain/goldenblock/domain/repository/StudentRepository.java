b3f53abe79373aadf1a31b02eca15b10a3d4e2f0
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-08-05 23:56:20 +0900

    add studentRepository at loadUserByusername

--- src/main/java/com/blockchain/goldenblock/domain/repository/StudentRepository.java
+++ src/main/java/com/blockchain/goldenblock/domain/repository/StudentRepository.java
@@ -5,5 +5,5 @@ import com.blockchain.goldenblock.domain.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long>{
-    
+    Student findByName(String name);
 }
