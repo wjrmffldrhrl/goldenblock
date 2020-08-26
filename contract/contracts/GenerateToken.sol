e9017b2bddaebb959b4457de167b867c10ceab9d
yuha <yuhatown@gmail.com>
2020-08-26 19:02:45 +0900

    error

--- contract/contracts/GenerateToken.sol
+++ contract/contracts/GenerateToken.sol
@@ -1,6 +1,6 @@
 pragma solidity <0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GenerateToken is ERC20 {
     
@@ -8,17 +8,17 @@ contract GenerateToken is ERC20 {
         _mint(msg.sender, 10000);
     }
     
-    function generate(address to, uint256 amount) public returns (bool){
+    function mint(address to, uint256 amount) public returns (bool){
         _mint(to, amount);
     }
     
    //골든 포인트 이동
-    function goldenTransfer(address from, address to, uint256 amount) public {
+    function sendToken(address from, address to, uint256 amount) public {
         _transfer(from, to, amount);
     }
 
     //골든 토큰 인출
-    function goldenBurn(address sender, uint256 amount) public{
+    function burn(address sender, uint256 amount) public{
         _burn(sender, amount);
     }
     
